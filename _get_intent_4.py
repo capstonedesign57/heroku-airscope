8e2edf28b8da0f5910396b69f0124746d5e0da68
heroku-airscope <capstonedesign57@gmail.com>
2019-10-06 00:29:17 +0900

    First commit for heroku-airscope

---
+++ _get_intent_4.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pickle
+
+def get_intent(sent):
+    # Flight/NoFlight
+    filename = 'MNB_model_1.sav'
+    MNB_model_load = pickle.load(open(filename, 'rb'))
+    dtmvector = pickle.load(open('dtmvector_1', 'rb'))
+    tfidf_transformer = pickle.load(open('tfidf_transformer_1', 'rb'))
+    
+    # 4_Intents (Only Flight/Cost/Airline)
+    filename_2 = 'MNB_model_2.sav'
+    MNB_model_load_2 = pickle.load(open(filename_2, 'rb'))
+    dtmvector_2 = pickle.load(open('dtmvector_2', 'rb'))
+    tfidf_transformer_2 = pickle.load(open('tfidf_transformer_2', 'rb'))
+    
+    test = []
+    test.append(sent)
+    test_dtm = dtmvector.transform(test)
+    tfidfv_test = tfidf_transformer.transform(test_dtm)
+    predict = MNB_model_load.predict(tfidfv_test)
+    
+    # 항공권 검색 질의인 경우
+    if predict == "Flight" : # 의도가 Flight일 때 세부 의도 파악하는 부분
+        test_dtm = dtmvector_2.transform(test)
+        tfidf_test = tfidf_transformer_2.transform(test_dtm)
+        predict = MNB_model_load_2.predict(tfidf_test)
+        return(predict)
+    
+    else :
+        return('NOT MY JOB')
+
