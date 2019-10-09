41513c631e10b950b9174b316e0dad5d374a621a
heroku-airscope <capstonedesign57@gmail.com>
2019-10-10 00:18:51 +0900

    Import mydb

---
+++ mydb.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pymysql
+
+
+# In[4]:
+
+
+def getdb():
+    mydb = pymysql.connect(
+    host = 'z1ntn1zv0f1qbh8u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port = 3306,
+    user = 'x3h6q2o87taceakh',
+    passwd = 'eh2lx3rowe8pty87',
+    db = 'xw6svbp8hrjpwvag',
+    charset = 'utf8',
+    autocommit = True)
+    
+    return mydb
+
