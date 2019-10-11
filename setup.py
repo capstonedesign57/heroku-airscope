2136d57c5de3a281d7b8533ad1ced32562ef2e7b
heroku-airscope <capstonedesign57@gmail.com>
2019-10-12 02:19:25 +0900

    Remove old setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(
     author='Airscope',
     author_email='capstonedesign57@gmail.com',
     install_requires=['keras_contrib'],
-    url=['https://github.com/keras-team/keras-contrib/tarball/master#egg=keras_contrib']
+    url=['https://github.com/keras-team/keras-contrib/tarball/master#egg=keras_contrib',
+    'https://github.com/huggingface/neuralcoref-models/releases/download/en_coref_md-3.0.0/en_coref_md-3.0.0.tar.gz']
 )
