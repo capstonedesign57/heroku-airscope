bbab02b4dc7325d26271ce94cc2df26371a7ed86
heroku-airscope <capstonedesign57@gmail.com>
2019-10-12 03:05:10 +0900

    Modify setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(
     name='heroku_code',
     author='Airscope',
     author_email='capstonedesign57@gmail.com',
-    install_requires=['keras-contrib @ git+https://www.github.com/keras-team/keras-contrib.git',
-    'en-coref-md @ https://github.com/huggingface/neuralcoref-models/releases/download/en_coref_md-3.0.0/en_coref_md-3.0.0.tar.gz'],
+    # install_requires=['keras-contrib @ git+https://www.github.com/keras-team/keras-contrib.git',
+    # 'en-coref-md @ https://github.com/huggingface/neuralcoref-models/releases/download/en_coref_md-3.0.0/en_coref_md-3.0.0.tar.gz'],
+    dependency_links=['git+https://www.github.com/keras-team/keras-contrib.git',
+    'https://github.com/huggingface/neuralcoref-models/releases/download/en_coref_md-3.0.0/en_coref_md-3.0.0.tar.gz'],
     url='https://github.com/capstonedesign57/heroku-airscope'
 )
