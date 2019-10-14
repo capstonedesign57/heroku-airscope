6352b38f44e8ec54ca5e8f34d05856e62b1876a1
heroku-airscope <capstonedesign57@gmail.com>
2019-10-15 01:45:50 +0900

    Very very tired...

--- _get_tag.py
+++ _get_tag.py
@@ -3,12 +3,11 @@
 
 # In[4]:
 
-
 from keras_contrib.layers import CRF
 from keras_contrib.losses import crf_loss
 from keras_contrib.metrics import crf_viterbi_accuracy
-from keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 
 # To load the model
 custom_objects={'CRF': CRF,'crf_loss':crf_loss,'crf_viterbi_accuracy':crf_viterbi_accuracy}
