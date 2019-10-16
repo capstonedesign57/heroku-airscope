93c558fcd247d4b54a9cb58bfa1cbcb81042f7ff
heroku-airscope <capstonedesign57@gmail.com>
2019-10-17 00:29:13 +0900

    Modify sql

--- _get_view.py
+++ _get_view.py
@@ -54,11 +54,11 @@ def getroute(intent, tagged, db):
 
     
     # create selectedRoute
-    #sql="CREATE OR REPLACE VIEW selectedRoute AS SELECT route_id, airline, airline_id, src_airport, src_name, src_city, src_country, dst_airport, dst_name, dst_city, dst_country, stops, dpt_time, DATE_ADD(dpt_time, INTERVAL TIME_TO_SEC(est_time) SECOND) as arr_time FROM joinedRoute WHERE "+fromloc+" AND "+toloc+" AND DATE(dpt_time)="+dpt_date
-    #curs.execute(sql)
+    sql="CREATE OR REPLACE VIEW selectedRoute AS SELECT route_id, airline, airline_id, src_airport, src_name, src_city, src_country, dst_airport, dst_name, dst_city, dst_country, stops, dpt_time, DATE_ADD(dpt_time, INTERVAL TIME_TO_SEC(est_time) SECOND) as arr_time FROM joinedRoute WHERE "+fromloc+" AND "+toloc+" AND DATE(dpt_time)="+dpt_date
+    curs.execute(sql)
     
-    sql="CREATE OR REPLACE VIEW selectedRoute AS SELECT route_id, airline, airline_id, src_airport, src_name, src_city, src_country, dst_airport, dst_name, dst_city, dst_country, stops, dpt_time, DATE_ADD(dpt_time, INTERVAL TIME_TO_SEC(est_time) SECOND) as arr_time FROM joinedRoute WHERE src_airport=\'"+fromloc+" AND dst_airport=\'"+toloc+" AND DATE(dpt_time)="+dpt_date
-    curs.execute(sql) # joinedRoute table이 없다고 나옴
+#    sql="CREATE OR REPLACE VIEW selectedRoute AS SELECT route_id, airline, airline_id, src_airport, src_name, src_city, src_country, dst_airport, dst_name, dst_city, dst_country, stops, dpt_time, DATE_ADD(dpt_time, INTERVAL TIME_TO_SEC(est_time) SECOND) as arr_time FROM joinedRoute WHERE src_airport=\'"+fromloc+" AND dst_airport=\'"+toloc+" AND DATE(dpt_time)="+dpt_date
+#    curs.execute(sql)
 
     # selectedRoute에 airline_name, cost JOIN
     sql2="CREATE OR REPLACE VIEW Route AS SELECT s.*, a.name as airline_name, c.cost FROM selectedRoute as s, airlines as a, cost as c WHERE s.airline_id=a.airline_id AND s.route_id=c.route_id"
