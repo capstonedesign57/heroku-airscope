a7ceee44766588298d982401b7aa7d99e23c04ab
heroku-airscope <capstonedesign57@gmail.com>
2019-10-23 04:55:00 +0900

    Modify sql

--- _get_view.py
+++ _get_view.py
@@ -61,7 +61,7 @@ def getroute(intent, tagged, db, bot, chat_id):
         SELECT route_id, airline, airline_id, src_airport, src_name, src_city, src_country, dst_airport, dst_name, dst_city, dst_country, stops, dpt_time, \
         DATE_ADD(dpt_time, INTERVAL TIME_TO_SEC(est_time) SECOND) as arr_time \
         FROM joinedRoute\
-        WHERE " + fromloc + " AND " + toloc + " AND DATE(dpt_time)=" + dpt_date
+        WHERE " + fromloc + " AND " + toloc
         # WHERE " + fromloc + " AND " + toloc + " AND DATE(dpt_time)=" + dpt_date
         # WHERE "+fromloc+" AND "+toloc+" AND DATE(dpt_time)="+dpt_date
     curs.execute(sql)
